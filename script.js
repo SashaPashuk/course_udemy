1c654c5ef1597f8be1c2096a64debde70b7e3a2f
Alexander_Pashuk <pashuk.sasha18@gmail.com>
2021-11-29 22:03:15 +0200

    task14

--- script.js
+++ script.js
@@ -76,3 +76,24 @@
 //         console.log("в точку")
 // }
  // TASK14-------------------------------------------------------------
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+
+//     if (i == 6 ) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i)
+// }
+
