6394a1ff26d6ae4d82ff8e7ed2f6bdaeffad0f98
Alexander_Pashuk <pashuk.sasha18@gmail.com>
2021-11-29 21:45:06 +0200

    task 13

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* Задание на урок:
+'use strict';
+/* Задание на урок: 12
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
@@ -21,25 +22,57 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
-// 1)
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
-// 2)
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-// 3)
-const a = prompt('Один из последних просмотренных фильмов?', ' '),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ' '),
-      d = prompt('На сколько оцените его?', '');
+// // 1)
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// console.log(numberOfFilms);
+// // 2)
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+// // 3)
+// const a = prompt('Один из последних просмотренных фильмов?', ' '),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ' '),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
 
+// TASK13-------------------------------------------------------------
+// if (4 == 4) {
+//     console.log('OK!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log("Багато");
+// } else {
+//     console.log('Ok!');
+// };
+
+// (num == 50) ? console.log('Ok!') : console.log('Error');
+
+
+// const num = 50;
+
+// switch (num) {
+//     case 49: 
+//         console.log("Невірно")
+//         break;
+//     case 100:
+//         console.log("Невірно")
+//         break;
+//     case 50:
+//         console.log("в точку")
+// }
+ // TASK14-------------------------------------------------------------
