d5026ff2f71bb189b4bc50ef0619deb9640bc2c6
Alexander_Pashuk <pashuk.sasha18@gmail.com>
2021-11-30 12:08:01 +0200

    add task19.js

---
+++ task19.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// function first() {
+//     // Do something
+//     setTimeout(function() {
+//         console.log(1)
+//     }, 500);
+// }
+
+// function second () {
+//     console.log(2)
+// };
+
+// first();
+// second();
+
+  function learnJS(lang, callback) {
+      console.log(`Я вчу: ${lang}`);
+      callback();
+  }
+  function done() {
+    console.log('Я пройшов даний урок!')
+  }
+
+  learnJS('JavaScript', done);
+
+
+  
