c1686d5213f754c8ecfacd52ca1d5e4ffd0d65d1
Alexander_Pashuk <pashuk.sasha18@gmail.com>
2021-11-30 10:01:48 +0200

    add task16.js

--- task16.js
+++ task16.js
@@ -1,1 +1,35 @@
 'use strict'
+
+let num = 10;
+function showFirstMessage(text) {
+    console.log(text);
+     console.log(num)
+}
+
+showFirstMessage("Hello World");
+console.log(num)
+
+function calc(a, b) {
+    return(a + b);
+}
+console.log(calc(4, 3));
+console.log(calc(3, 3));
+console.log(calc(5, 3));
+
+
+
+function ret () {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret()
+console.log(anotherNum) 
+
+const logger = function() {
+  console.log('Hello')
+};
+
+logger()
+
+const calc = (a, b) => a + b;
