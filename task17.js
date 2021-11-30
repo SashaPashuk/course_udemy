eb115691e092f2391b9b9af0214c09fc1496bab0
Alexander_Pashuk <pashuk.sasha18@gmail.com>
2021-11-30 10:30:19 +0200

    add task17.js

---
+++ task17.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const str = "test";
+const arr = [1, 2, 4]
+
+// console.log(str[2]);
+console.log(str.toLocaleUpperCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('fruit'))
+
+const logg = "Hello World";
+
+// console.log(logg.slice(6));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test))
